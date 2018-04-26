589028a46f45d1e8d090363018dacc4a00ee9dab
daidahao <11510415@mail.sustc.edu.cn>
2018-04-27 00:12:32 +0800

    disable support for mlfqs

--- src/threads/init.c
+++ src/threads/init.c
@@ -255,7 +255,8 @@ parse_options (char **argv)
       else if (!strcmp (name, "-rs"))
         random_init (atoi (value));
       else if (!strcmp (name, "-mlfqs"))
-        thread_mlfqs = true;
+        PANIC("does not support mlfqs");
+        // thread_mlfqs = true;
 #ifdef USERPROG
       else if (!strcmp (name, "-ul"))
         user_page_limit = atoi (value);
